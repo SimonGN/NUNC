c2d35bacf941b8f3b39077b15d58efa7913f2b5e
Simon Gonzalez Negrete <simongonzaleznegrete90@gmail.com>
2019-06-06 15:23:20 +0100

    coomit error🏗

--- components/Intro/Intro.js
+++ components/Intro/Intro.js
@@ -2,15 +2,25 @@ import React from "react";
 import { IntroStyle } from "./IntroStyle";
 
 import logoNunc from "../../static/svg/Logo.svg";
-import FontpHome from "../../pagesStyles/fontStyle/pHome";
+import AppStore from "../../static/svg/IconAppstore.svg"
+import Googleplay from "../../static/svg/IconGoogleplay.svg"
+
+import h1 from "../../styles/pagesStyles/h1";
 
 const Intro = props => {
     return (
         
         <IntroStyle>
+            <img className="logoNUNC" src={logoNunc}></img>
             <div className="homeBack">
-                <img className="logoNUNC" src={logoNunc}></img>
-                <FontpHome color="white">Creating meaningful products</FontpHome>
+                <div>
+                    <h1 color="white">Cumple con las obligaciones de marcaje sin que nada cambie en tu empresa.</h1>
+                </div>
+                <div>
+                    <img src={AppStore}></img>
+                    <img src={Googleplay}></img>
+                </div>
+                <img src="../../static/img/iphoneIntro.png"></img>
             </div>
         </IntroStyle>
 
