ec6f74fd275d3886f7cb0b90bcbfb482be73cda1
diegomendezp <diegomen15@gmail.com>
2019-06-06 14:17:31 +0200

    Meta component fixed 🚧

--- components/Meta.js
+++ components/Meta.js
@@ -5,45 +5,47 @@ import Head from "next/head";
 
 const Meta = () => {
   return (
-  <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta charSet="utf-8" />
-      <title>NU:NC</title>
-    </Head>
-    <style jsx global>{`
-    
-      
-    @font-face {
-        font-family: "KareliaWeb-Regular";
-        src: url(${KareliaWeb}) format("truetype");
-        font-weight: normal;
-        font-style: normal;
-    }
-    @font-face {
-        font-family: "Texgyreheroscn";
-        src: url(${Texg}) format("truetype");
-        font-weight: normal;
-        font-style: normal;
-    }
-    * {
-        box-sizing:border-box;
-    }
-      
-      body {
-        margin: 0;
-        padding: 0;
-        max-width: 100%;
-      }
-      h1, h2, h3 p {   
-        margin-top: 0;
-        margin-bottom: 0;
-      }
+    <React.Fragment>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <title>NU:NC</title>
+      </Head>
+      <style jsx global>{`
+        @font-face {
+          font-family: "KareliaWeb-Regular";
+          src: url(${KareliaWeb}) format("truetype");
+          font-weight: normal;
+          font-style: normal;
+        }
+        @font-face {
+          font-family: "Texgyreheroscn";
+          src: url(${Texg}) format("truetype");
+          font-weight: normal;
+          font-style: normal;
+        }
+        * {
+          box-sizing: border-box;
+        }
 
-      .* {
-        box-sizing: border-box;
-      }
-    `}</style>
-  )
-}
+        body {
+          margin: 0;
+          padding: 0;
+          max-width: 100%;
+        }
+        h1,
+        h2,
+        h3 p {
+          margin-top: 0;
+          margin-bottom: 0;
+        }
+
+        .* {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </React.Fragment>
+  );
+};
 
 export default Meta;
