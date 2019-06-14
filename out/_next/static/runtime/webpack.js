cff61f32e6aa74eebe3fd15d91330c3897af886f
SimonGN <simongonzaleznegrete90@gmail.com>
2019-06-14 11:21:40 +0200

    Deploy Next.js to gh-pages

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "47ba94cecdc2b5fd41b6";
+/******/ 	var hotCurrentHash = "9928456d101dbdac86ab";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
