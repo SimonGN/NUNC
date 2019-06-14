70a79d78caf7f1a2c41b545708afd57b58290054
SimonGN <simongonzaleznegrete90@gmail.com>
2019-06-14 09:52:54 +0200

    Deploy gh-pages

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d7499cd6cb5c4eae58c9";
+/******/ 	var hotCurrentHash = "800ce4771da8111bbf6a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ _next/static/webpack/static/development/pages/index.js.4fb5a5bd17aef635caa2.hot-update.js
@@ -0,0 +1,121 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/Intro/IconAppstore.svg":
+false,
+
+/***/ "./components/Intro/IconGooleplay.svg":
+false,
+
+/***/ "./components/Intro/Intro.js":
+/*!***********************************!*\
+  !*** ./components/Intro/Intro.js ***!
+  \***********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _IntroStyle__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./IntroStyle */ "./components/Intro/IntroStyle.js");
+/* harmony import */ var _styles_pagesStyles_Fonth1__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../styles/pagesStyles/Fonth1 */ "./styles/pagesStyles/Fonth1.js");
+var _jsxFileName = "/Users/simon/Desktop/LandingNUNC/components/Intro/Intro.js";
+
+
+
+
+var Intro = function Intro(props) {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_IntroStyle__WEBPACK_IMPORTED_MODULE_1__["IntroStyle"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "intro",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "home",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    className: "logoNUNC",
+    src: "/static/svg/Logo.svg",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "text",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 13
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_styles_pagesStyles_Fonth1__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 14
+    },
+    __self: this
+  }, "Cumple con las obligaciones de marcaje sin que nada cambie en tu empresa.")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "stores",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 16
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    className: "appStore",
+    src: "/static/svg/IconAppstore.svg",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 17
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    className: "googleplay",
+    src: "/static/svg/IconGooleplay.svg",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 18
+    },
+    __self: this
+  }))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "iphone",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 21
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    className: "movil",
+    src: "/static/img/movilNunc.png",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 22
+    },
+    __self: this
+  }))));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Intro);
+
+/***/ }),
+
+/***/ "./components/Intro/Logo.svg":
+false,
+
+/***/ "./components/Intro/movilNunc.png":
+false
+
+})
+//# sourceMappingURL=index.js.4fb5a5bd17aef635caa2.hot-update.js.map
